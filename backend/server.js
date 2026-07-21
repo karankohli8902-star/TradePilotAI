8a10f1cd9cde5dc2dabf91b549dd96cbd798609b
Karan Kohli <karankohli8902@gmail.com>
2026-07-21 14:26:02 +0530

    Frontend connected with backend API

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-console.log("🚀 TradePilot AI Backend Started");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/market", (req, res) => {
+  res.json({
+    nifty: {
+      price: "24,350",
+      trend: "Bullish",
+      signal: "BUY",
+      entry: "24,360",
+      stopLoss: "24,300",
+      target: "24,500",
+    },
+
+    sensex: {
+      price: "80,150",
+      trend: "Bullish",
+      signal: "BUY",
+      entry: "80,200",
+      stopLoss: "80,050",
+      target: "80,500",
+    },
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
